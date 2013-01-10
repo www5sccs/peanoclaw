8e95d47a272ab3d3edfc0ffcb042bcf9410ec80a
Arash Bakhtiari <bakhtiar@in.tum.de>
2013-01-10 15:00:29 +0100

    Added the result verification script for refined example

---
+++ apps/peano_refinded_acoustic_3d_heterogeneous/assembleState.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+def assemblePeanoClawState(peanoClawSolution, globalDomain, num_eqn, mgrid):
+  import numpy
+  from clawpack.pyclaw.geometry import Dimension, Domain
+  from clawpack.pyclaw.state import State
+
+  dimensions = []
+  for d in xrange(len(globalDomain.patch.dimensions)):
+    dimensions.append(Dimension(globalDomain.patch.dimensions[d].name, globalDomain.patch.dimensions[d].lower, globalDomain.patch.dimensions[d].upper, \
+    mgrid))
+    print globalDomain.patch.dimensions[d]
+
+  assembledPeanoClawState = State(Domain(dimensions), num_eqn)
+  assembledPeanoClawState.q[0,:,:,:] = 0.0
+  
+  for peanoClawState in peanoClawSolution.gathered_states:
+    offsetX = int((peanoClawState.patch.dimensions[0].lower - assembledPeanoClawState.patch.dimensions[0].lower) / assembledPeanoClawState.patch.dimensions[0].delta )
+    offsetY = int((peanoClawState.patch.dimensions[1].lower - assembledPeanoClawState.patch.dimensions[1].lower) /
+     assembledPeanoClawState.patch.dimensions[1].delta )
+    offsetZ = int((peanoClawState.patch.dimensions[2].lower - assembledPeanoClawState.patch.dimensions[2].lower) /
+     assembledPeanoClawState.patch.dimensions[2].delta )
+    for d in xrange(len(globalDomain.patch.dimensions)):
+      print peanoClawState.patch.dimensions[d]
+    print "PEANO STATE" , peanoClawState.q.shape
+    print "OFFSET", offsetX,' ', offsetY,' ', offsetZ
+    print "assemble", assembledPeanoClawState.q.shape
+    assembledPeanoClawState.q[0, offsetX:offsetX+peanoClawState.q.shape[1], offsetY:offsetY+peanoClawState.q.shape[2],offsetZ:offsetZ+peanoClawState.q.shape[3]] = peanoClawState.q[0, :, :, :]
+    
+  # if numpy.min(assembledPeanoClawState.q[0, :, :,:]) < 0.0:
+  #   raise Exception("Not all cells have been filled!")
+    
+  return assembledPeanoClawState.q[0,:,:,:]
+
+
+
---
+++ apps/peano_refinded_acoustic_3d_heterogeneous/verify_acoustics.py
@@ -0,0 +1,339 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import numpy as np
+import clawpack.peanoclaw as peanoclaw
+import wavefront as wf
+from clawpack import pyclaw
+
+OBJECTS = None
+XI = -1.0
+XF = 1.0
+LENGTH = XF - XI
+SUBDIVISION_FACTOR = 6
+CELLS = 3
+INIT_MIN_MESH_WIDTH = LENGTH / CELLS / SUBDIVISION_FACTOR
+NUM_OUTPUT_TIMES = 2
+TEST_LEVEL = 5
+NUM_EQS = 4
+TFINAL = 1.0
+def init_objects(filename):
+    global OBJECTS
+    OBJECTS = wf.read_obj(filename)
+
+def init_aux(state):
+    global OBJECTS
+    zr = 1.0  # Impedance in right half
+    cr = 1.0  # Sound speed in right half
+
+    zl = 1.0  # Impedance in left half
+    cl = 1.0  # Sound speed in left half
+    grid = state.grid
+    grid.compute_c_centers()
+    X,Y,Z = grid._c_centers
+
+    if OBJECTS is not None:
+        vertices = OBJECTS[0].vertices
+        vert_x = [vert[0] for vert in vertices ]
+        vert_y = [vert[1] for vert in vertices ]
+        vert_z = [vert[2] for vert in vertices ]
+        XMAX = max(vert_x)
+        XMIN = min(vert_x)
+        YMAX = max(vert_y)
+        YMIN = min(vert_y)
+        ZMAX = max(vert_z)
+        ZMIN = min(vert_z)
+    
+        # print 'XMIN',  XMIN
+        # print 'XMAX',  XMAX
+        inside_the_object = np.logical_and(X > XMIN , X < XMAX )#, Y > YMIN, Y < YMAX, Z > ZMIN, Z < ZMAX)
+        state.aux[0,:,:,:] = zl*(np.logical_not(inside_the_object)) + zr*(inside_the_object) # Impedance
+        state.aux[1,:,:,:] = cl*(np.logical_not(inside_the_object)) + cr*(inside_the_object) # Sound speed
+    else:
+        border = 0.0#0.35
+        state.aux[0,:,:,:] = zl*(X<border) + zr*(X>=border) # Impedance
+        state.aux[1,:,:,:] = cl*(X<border) + cr*(X>=border) # Sound speed
+
+def init_q(state):
+    grid = state.grid
+    grid.compute_c_centers()
+    X,Y,Z = grid._c_centers
+
+    x0 = -0.5; y0 = 0.0; z0 = 0.0
+    r = np.sqrt((X-x0)**2 + (Y-y0)**2 + (Z-z0)**2)
+    width=0.07
+    state.q[0,:,:,:] = (np.abs(r-0.3)<=width)*(1.+np.cos(np.pi*(r-0.3)/width))
+    state.q[1,:,:,:] = 0.
+    state.q[2,:,:,:] = 0.
+    state.q[3,:,:,:] = 0.
+
+def init(state):
+    init_aux(state)
+    init_q(state)
+
+def refinement_criterion(state):
+    global SUBDIVISION_FACTOR
+    global XI
+    global XF
+    global LENTGH
+    global CELLS
+    global INIT_MIN_MESH_WIDTH
+    # grid = state.grid;
+    # num_dim = grid.num_dim
+    # delta = grid.delta
+    # xwidth = delta[0]
+    if state.aux[0,:,:,:].max() > 1:
+        return INIT_MIN_MESH_WIDTH / 2.0
+ 
+    return INIT_MIN_MESH_WIDTH
+ 
+def acoustics3D(iplot=False,htmlplot=False,use_petsc=False,outdir='./_output',solver_type='classic',**kwargs):
+    """
+    Example python script for solving the 3d acoustics equations.
+    """
+    #===========================================================================
+    # Import libraries
+    #===========================================================================
+    global INIT_MIN_MESH_WIDTH
+    global NUM_OUTPUT_TIMES
+    global TFINAL
+    if use_petsc:
+        import clawpack.petclaw as pyclaw
+    else:
+        from clawpack import pyclaw
+
+    #===========================================================================
+    # Setup solver and solver parameters
+    #===========================================================================
+    if solver_type=='classic':
+        solver=pyclaw.ClawSolver3D()
+    else:
+        raise Exception('Unrecognized solver_type.')
+
+    from clawpack import riemann
+
+    # Peano Solver
+    peanoSolver = peanoclaw.Solver(solver,
+                                   INIT_MIN_MESH_WIDTH,
+                                   init, 
+                                   refinement_criterion=refinement_criterion
+                                   )
+
+
+    solver.rp = riemann.rp3_vc_acoustics
+    solver.num_waves = 2
+    solver.limiters = pyclaw.limiters.tvd.MC
+
+    solver.bc_lower[0]=pyclaw.BC.extrap
+    solver.bc_upper[0]=pyclaw.BC.extrap
+    solver.bc_lower[1]=pyclaw.BC.extrap
+    solver.bc_upper[1]=pyclaw.BC.extrap
+    solver.bc_lower[2]=pyclaw.BC.extrap
+    solver.bc_upper[2]=pyclaw.BC.extrap
+
+    solver.aux_bc_lower[0]=pyclaw.BC.extrap
+    solver.aux_bc_upper[0]=pyclaw.BC.extrap
+    solver.aux_bc_lower[1]=pyclaw.BC.extrap
+    solver.aux_bc_upper[1]=pyclaw.BC.extrap
+    solver.aux_bc_lower[2]=pyclaw.BC.extrap
+    solver.aux_bc_upper[2]=pyclaw.BC.extrap
+    solver.dimensional_split=True
+    solver.limiters = pyclaw.limiters.tvd.MC
+
+    #===========================================================================
+    # Initialize domain and state, then initialize the solution associated to the 
+    # state and finally initialize aux array
+    #===========================================================================
+
+    # Initialize domain
+    mx = SUBDIVISION_FACTOR
+    my = SUBDIVISION_FACTOR
+    mz = SUBDIVISION_FACTOR
+    x = pyclaw.Dimension('x', XI, XF, mx)
+    y = pyclaw.Dimension('y', XI, XF, my)
+    z = pyclaw.Dimension('z', XI, XF, mz)
+    domain = pyclaw.Domain([x,y,z])
+
+    num_eqn = 4
+    num_aux = 2 # density, sound speed
+    state = pyclaw.State(domain,num_eqn,num_aux)
+
+    #===========================================================================
+    # Set up controller and controller parameters
+    #===========================================================================
+    claw = pyclaw.Controller()
+    claw.tfinal = TFINAL
+    claw.keep_copy = True
+    claw.solution = peanoclaw.solution.Solution(state,domain) #pyclaw.Solution(state,domain)
+    claw.solver = peanoSolver  #solver
+    claw.outdir=outdir
+    claw.num_output_times = NUM_OUTPUT_TIMES
+
+    #===========================================================================
+    # Solve the problem
+    #===========================================================================
+    # def _probe( solver , solution):
+
+    #     dim_x = solution.states[0].patch.dimensions[0]
+    #     dim_y = solution.states[0].patch.dimensions[1]
+    #     dim_z = solution.states[0].patch.dimensions[2]
+        
+    #     if abs( dim_x.lower + 1./3. ) < 1e-8 and abs(dim_y.lower + 1./3. ) < 1e-8 and abs( dim_z.lower - 1./3. ) < 1e-8:
+    #         print "PROBE"
+    #         print solver.qbc[0,:,3,:]
+
+    #     if abs( dim_x.lower - 1./3. ) < 1e-8 and abs(dim_y.lower + 1./3. ) < 1e-8 and abs( dim_z.lower + 1./3. ) < 1e-8:
+    #         print "PROBE1"
+    #         print solver.qbc[0,:,3,:]
+
+        
+
+    #solver.before_step = _probe
+    status = claw.run()
+
+    #===========================================================================
+    # Plot results
+    #===========================================================================
+    #if htmlplot:  pyclaw.plot.html_plot(outdir=outdir,file_format=claw.output_format)
+    #if iplot:     pyclaw.plot.interactive_plot(outdir=outdir,file_format=claw.output_format)
+    return claw
+
+def pyclaw_acoustics3D(iplot=False,htmlplot=False,use_petsc=False,outdir='./_output',solver_type='classic',**kwargs):
+    """
+    Example python script for solving the 3d acoustics equations.
+    """
+    global NUM_OUTPUT_TIMES
+    global TFINAL
+    if use_petsc:
+        import clawpack.petclaw as pyclaw
+    else:
+        from clawpack import pyclaw
+
+    if solver_type=='classic':
+        solver=pyclaw.ClawSolver3D()
+    else:
+        raise Exception('Unrecognized solver_type.')
+
+    from clawpack import riemann
+    solver.rp = riemann.rp3_vc_acoustics
+    solver.num_waves = 2
+    solver.limiters = pyclaw.limiters.tvd.MC
+
+    solver.bc_lower[0]=pyclaw.BC.extrap
+    solver.bc_upper[0]=pyclaw.BC.extrap
+    solver.bc_lower[1]=pyclaw.BC.extrap
+    solver.bc_upper[1]=pyclaw.BC.extrap
+    solver.bc_lower[2]=pyclaw.BC.extrap
+    solver.bc_upper[2]=pyclaw.BC.extrap
+
+    solver.aux_bc_lower[0]=pyclaw.BC.extrap
+    solver.aux_bc_upper[0]=pyclaw.BC.extrap
+    solver.aux_bc_lower[1]=pyclaw.BC.extrap
+    solver.aux_bc_upper[1]=pyclaw.BC.extrap
+    solver.aux_bc_lower[2]=pyclaw.BC.extrap
+    solver.aux_bc_upper[2]=pyclaw.BC.extrap
+    solver.dimensional_split=True
+    solver.limiters = pyclaw.limiters.tvd.MC
+
+    #===========================================================================
+    # Initialize domain and state, then initialize the solution associated to the 
+    # state and finally initialize aux array
+    #===========================================================================
+    # Initialize domain
+    mx = SUBDIVISION_FACTOR * CELLS
+    my = SUBDIVISION_FACTOR * CELLS
+    mz = SUBDIVISION_FACTOR * CELLS
+    x = pyclaw.Dimension('x', XI, XF, mx)
+    y = pyclaw.Dimension('y', XI, XF, my)
+    z = pyclaw.Dimension('z', XI, XF, mz)
+    domain = pyclaw.Domain([x,y,z])
+
+    num_eqn = 4
+    num_aux = 2 # density, sound speed
+    state = pyclaw.State(domain,num_eqn,num_aux)
+    init(state)
+
+    claw = pyclaw.Controller()
+    claw.keep_copy = True
+    claw.solution = pyclaw.Solution(state,domain)
+    claw.solver = solver
+    claw.outdir=outdir
+    claw.num_output_times = NUM_OUTPUT_TIMES
+
+    # Solve
+    claw.tfinal = TFINAL
+    status = claw.run()
+
+    # if htmlplot:  pyclaw.plot.html_plot(outdir=outdir,file_format=claw.output_format)
+    # if iplot:     pyclaw.plot.interactive_plot(outdir=outdir,file_format=claw.output_format)
+
+    # pinitial=claw.frames[0].state.get_q_global()
+    # pmiddle  =claw.frames[3].state.get_q_global()
+    # pfinal  =claw.frames[claw.num_output_times].state.get_q_global()
+
+    return claw
+    # if pinitial != None and pmiddle != None and pfinal != None:
+    #     pinitial =pinitial[0,:,:,:].reshape(-1)
+    #     pmiddle  =pmiddle[0,:,:,:].reshape(-1)
+    #     pfinal   =pfinal[0,:,:,:].reshape(-1)
+    #     final_difference =np.prod(grid.delta)*np.linalg.norm(pfinal-pinitial,ord=1)
+    #     middle_difference=np.prod(grid.delta)*np.linalg.norm(pmiddle-pinitial,ord=1)
+
+    #     if app == None:
+    #         print 'Final error: ', final_difference
+    #         print 'Middle error: ', middle_difference
+
+    #     # import matplotlib.pyplot as plt
+    #     # for i in range(claw.num_output_times):
+    #     #    plt.pcolor(claw.frames[i].state.q[0,:,:,mz/2])
+    #     #    plt.figure()
+    #     # plt.show()
+
+    #     return pfinal, final_difference
+
+    # else:
+        
+    #     return
+
+def verify(claw1, claw2):
+    global NUM_OUTPUT_TIMES
+    global TEST_LEVEL
+    global NUM_EQS
+    global CELLS
+    global SUBDIVISION_FACTOR
+    import assembleState as AS
+
+    qinit1=claw1.frames[0].state.get_q_global()
+    qinit2=claw1.frames[0].state.get_q_global()
+
+    print 'LENGH1:', len(qinit1)
+    print 'LENGH2:', len(qinit2)
+    import matplotlib.pyplot as plt
+    # for i in range(claw1.num_output_times):
+    #         qinit1=claw1.frames[i].state.get_q_global()
+    #         qinit2=claw1.frames[i].state.get_q_global()
+    #         pinit1 = qinit1[0,:,:,:].reshape(-1)
+    #         pinit2 = qinit2[0,:,:,:].reshape(-1)
+    #         import matplotlib.pyplot as plt
+
+    for i in range(NUM_OUTPUT_TIMES):
+        print 'TYPE', type(claw1.frames[i])
+        q_peano = AS.assemblePeanoClawState(claw1.frames[i], claw2.frames[i].domain, NUM_EQS, CELLS * SUBDIVISION_FACTOR)
+        res1 = q_peano[:,:,TEST_LEVEL]
+        res2 = claw2.frames[i].state.q[0,:,:,TEST_LEVEL]
+        if len(res1) != len(res2):
+            print "FAILED LOSER!"
+            return
+        plt.pcolor(res2 - res1)
+        plt.figure()
+        plt.show()
+            
+
+
+if __name__=="__main__":
+    import sys
+    from clawpack.pyclaw.util import run_app_from_main
+    #init_objects('cube.obj')
+    pyclaw_output = run_app_from_main(pyclaw_acoustics3D)
+    peanoclaw_output = run_app_from_main(acoustics3D)
+    verify( peanoclaw_output, pyclaw_output)
