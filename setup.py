c186007a5cb4a882eea0d4c3b3d1877cef68f7d8
Kristof Unterweger <unterweg@in.tum.de>
2013-03-18 15:10:00 +0100

    fixed

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ def setup_package():
             setup(**setup_dict)
             
             from subprocess import call
-            call("ls src/peanoclaw") #"svn checkout svn://svn.code.sf.net/p/peano/code/trunk src/peanoclaw/p3")
+            #call("ls src/peanoclaw") #"svn checkout svn://svn.code.sf.net/p/peano/code/trunk src/peanoclaw/p3")
             
             return
 
