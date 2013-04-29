e20fd786d336e738ee04c7efb328be2a371556dc
Kristof Unterweger <unterweg@in.tum.de>
2013-04-29 17:20:22 +0200

    changed generation

--- generatePeanoClaw.sh
+++ generatePeanoClaw.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./generateHeapData.sh
 
-java -jar ../p3/pdt/pdt.jar peanoclaw.peano-specification src/peanoclaw
+java -jar ../p3/pdt/pdt.jar peanoclaw.peano-specification src/peanoclaw src/peanoclaw
