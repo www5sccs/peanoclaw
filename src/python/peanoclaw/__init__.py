12241636f1fb270f8e64c754caa2e0858bb8c5f3
unterweg <unterweg@in.tum.de>
2013-07-29 16:18:46 +0200

    fixed modules

--- src/python/peanoclaw/__init__.py
+++ src/python/peanoclaw/__init__.py
@@ -4,4 +4,4 @@ __all__.extend(['Solver', 'Solution', 'State', 'SubgridSolver'])
 from peanoclaw.solver import Solver
 from peanoclaw.solution import Solution
 from peanoclaw.subgridsolver import SubgridSolver
-from pyclaw.state import State
+from clawpack.pyclaw.state import State
