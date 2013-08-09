bf8214ea6bfbdbdada278f02182d0cc1859a5bd5
unterweg <unterweg@in.tum.de>
2013-08-09 08:49:08 +0200

    added error case

---
+++ testscenarios/breakingDam/filterOutput.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+egrep -n "Vertex. after creating cell at \[0.6913.*,0.567901\]|Setting persistent vertex.*x:\[0.703704,0.567901\]|Setting hanging.*\[0.703704,0.567901\]|Setting cellDescriptionIndex\(.*\) from 2688 to 264|Destroying cell.*\[0.691358,0.567901\]" output
