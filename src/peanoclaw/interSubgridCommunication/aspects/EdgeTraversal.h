410c6ccff117a45ad50138a35816a55e448aa8f7
unterweg <unterweg@in.tum.de>
2014-02-12 10:00:46 +0100

    added EdgeTraversal

---
+++ src/peanoclaw/interSubgridCommunication/aspects/EdgeTraversal.h
@@ -0,0 +1,42 @@
+/*
+ * EdgeTraversal.h
+ *
+ *  Created on: Feb 10, 2014
+ *      Author: kristof
+ */
+
+#ifndef PEANOCLAW_INTERSUBGRIDCOMMUNICATION_ASPECTS_EDGETRAVERSAL_H_
+#define PEANOCLAW_INTERSUBGRIDCOMMUNICATION_ASPECTS_EDGETRAVERSAL_H_
+
+namespace peanoclaw {
+  namespace interSubgridCommunication {
+    namespace aspects {
+      template<class LoopBody>
+      class EdgeTraversal;
+    }
+  }
+}
+
+template<class LoopBody>
+class peanoclaw::interSubgridCommunication::aspects::EdgeTraversal {
+  public:
+    /**
+     * Traverses all 12 edges (in 3D) or 4 corners of the given subgrid.
+     *
+     * The LoopBody has to provide an operator() with the following parameters:
+     *
+     *  - peanoclaw::Patch: The subgrid on which the traversal is performed
+     *  - peanoclaw::Area: An area describing the ghostlayer edge of the subgrid.
+     *  - tarch::la::Vector<DIMENSIONS, int>: Describing the direction of the edge.
+     *              The dimension which axis is parallel to the edge is set to zero.
+     */
+    EdgeTraversal(
+      Patch& subgrid,
+      LoopBody& loopBody
+    );
+};
+
+
+#include "peanoclaw/interSubgridCommunication/aspects/EdgeTraversal.cpph"
+
+#endif /* PEANOCLAW_INTERSUBGRIDCOMMUNICATION_ASPECTS_EDGETRAVERSAL_H_ */
