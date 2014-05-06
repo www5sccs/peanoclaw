2355e82ac2a7d8b1f4d475ebe80f16b6ade0b7e8
unterweg <unterweg@in.tum.de>
2014-05-06 11:32:24 +0200

    disabled timeout warning

--- src/peanoclaw/runners/PeanoClawLibraryRunner.cpp
+++ src/peanoclaw/runners/PeanoClawLibraryRunner.cpp
@@ -58,8 +58,8 @@ void peanoclaw::runners::PeanoClawLibraryRunner::initializePeano(
 void peanoclaw::runners::PeanoClawLibraryRunner::initializeParallelEnvironment() {
   //Distributed Memory
   #if defined(Parallel)
-  tarch::parallel::Node::getInstance().setTimeOutWarning(4500);
-  tarch::parallel::Node::getInstance().setDeadlockTimeOut(9000);
+//  tarch::parallel::Node::getInstance().setTimeOutWarning(4500);
+//  tarch::parallel::Node::getInstance().setDeadlockTimeOut(9000);
 
   if (tarch::parallel::Node::getInstance().isGlobalMaster()) {
     tarch::parallel::NodePool::getInstance().setStrategy( new tarch::parallel::FCFSNodePoolStrategy() );
