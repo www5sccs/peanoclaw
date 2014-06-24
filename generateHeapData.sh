52bcc8082cd1c19760506337e68d82845c883251
unterweg <unterweg@in.tum.de>
2014-06-24 09:50:25 +0200

    added modified version

--- generateHeapData.sh
+++ generateHeapData.sh
@@ -2,7 +2,8 @@ PDT_PATH=../peano3/pdt
 
 java -jar $PDT_PATH/lib/DaStGen.jar --inline --plugin PeanoHeapSnippetGenerator --naming PeanoHeapNameTranslator --include $PWD/../../src src/peanoclaw/dastgen/CellDescription.def src/peanoclaw/records/
 java -jar $PDT_PATH/lib/DaStGen.jar --inline --plugin PeanoHeapSnippetGenerator --naming PeanoHeapNameTranslator --include $PWD/../../src src/peanoclaw/dastgen/VertexDescription.def src/peanoclaw/records/
-java -jar $PDT_PATH/lib/DaStGen.jar --inline --plugin PeanoHeapSnippetGenerator --naming PeanoHeapNameTranslator --include $PWD/../../src src/peanoclaw/dastgen/Data.def src/peanoclaw/records/
+java -jar $PDT_PATH/lib/DaStGen.jar --inline --plugin PeanoHeapSnippetGenerator --naming PeanoHeapNameTranslator --include $PWD/../../src src/peanoclaw/dastgen/DoubleData.def src/peanoclaw/records/
+java -jar $PDT_PATH/lib/DaStGen.jar --inline --plugin PeanoHeapSnippetGenerator --naming PeanoHeapNameTranslator --include $PWD/../../src src/peanoclaw/dastgen/FloatData.def src/peanoclaw/records/
 java -jar $PDT_PATH/lib/DaStGen.jar --inline --plugin PeanoHeapSnippetGenerator --naming PeanoHeapNameTranslator --include $PWD/../../src src/peanoclaw/dastgen/PatchDescription.def src/peanoclaw/records/
 java -jar $PDT_PATH/lib/DaStGen.jar --inline --plugin PeanoHeapSnippetGenerator --naming PeanoHeapNameTranslator --include $PWD/../../src src/peanoclaw/dastgen/LevelStatistics.def src/peanoclaw/statistics/
 java -jar $PDT_PATH/lib/DaStGen.jar --inline --plugin PeanoHeapSnippetGenerator --naming PeanoHeapNameTranslator --include $PWD/../../src src/peanoclaw/dastgen/TimeIntervalStatistics.def src/peanoclaw/statistics/
