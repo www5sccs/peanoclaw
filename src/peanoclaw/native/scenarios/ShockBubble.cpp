415945a984f4c7a82306ebadd97b1e2f417f3c2e
unterweg <unterweg@in.tum.de>
2014-11-20 14:53:56 +0100

    fixed

--- src/peanoclaw/native/scenarios/ShockBubble.cpp
+++ src/peanoclaw/native/scenarios/ShockBubble.cpp
@@ -133,10 +133,10 @@ peanoclaw::native::scenarios::ShockBubble::ShockBubble(
   }
 
   double finestSubgridTopologyPerDimension = atof(arguments[0].c_str());
-  _minimalMeshWidth = _domainSize[0] / finestSubgridTopologyPerDimension;
+  _minimalMeshWidth = tarch::la::Vector<DIMENSIONS,double>(_domainSize[0] / finestSubgridTopologyPerDimension);
 
   double coarsestSubgridTopologyPerDimension = atof(arguments[1].c_str());
-  _maximalMeshWidth = _domainSize[0] / coarsestSubgridTopologyPerDimension;
+  _maximalMeshWidth = tarch::la::Vector<DIMENSIONS,double>(_domainSize[0] / coarsestSubgridTopologyPerDimension);
 
   _endTime = atof(arguments[3].c_str());
 
